e8e8ec293d1b9133a6cc0c5c56d98d57b5c80707
Brian Mount <bvmount@gmail.com>
2012-03-31 20:23:44 -0700

    temp -- redirect to list of edits on save

--- mapcouchapp/updates/content.js
+++ mapcouchapp/updates/content.js
@@ -15,11 +15,9 @@ function (doc, req) {
     doc.content = JSON.parse(req.form.content);
     
     var appURL = '/' + req.info.db_name + '/' + ddoc._id,
-      //  docURL = appURL + '/_show/editor/' + doc._id;
-      docListUrl = appUrl + ' _list/summary/by_moddate_cp'    
+        docListURL = appURL + '/_list/summary/by_moddate_cp';
       return [doc, { 
-     //   headers: {'Location': docURL}, code: 303,
-        headers: {'Location': docListURL}, code: 302,
-       // body: fs.to_html(ddoc.templates.update_content, {docURL:docURL})
+        headers: {'Location': docListURL}, code: 303,
+        body: ""
     }];
 }
